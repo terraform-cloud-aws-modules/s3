2f196a30bfc2464cc8f43233b1528ad26ae470e7
Dan <me.daniel.meier@gmail.com>
2020-10-23 15:35:04 +0200

    cleanup2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # combine variable lists into 'folderstructure'
 locals {
-  folderstructure = "${combine(var.rootlevelfolder, var.sublevelfolder)}"
+  folderstructure = combine(var.rootlevelfolder, var.sublevelfolder)
 }
 
 # create s3 bucket
@@ -15,9 +15,9 @@ resource "aws_s3_bucket" "this" {
 
 # generate root and subfolders
 resource "aws_s3_bucket_object" "this" {
-	  count					= "${length(var.rootlevelfolder) * length(var.sublevelfolder)}"
+	  count					= length(var.rootlevelfolder) * length(var.sublevelfolder)
     bucket				= aws_s3_bucket.this.id
     acl						= var.acl
-    key						= "${element(local.folderstructure, count.index)[0]}/${element(local.folderstructure, count.index)[1]}"
+    key						= element(local.folderstructure, count.index)[0]}/${element(local.folderstructure, count.index)[1]
     content_type	= "application/x-directory"
 }
