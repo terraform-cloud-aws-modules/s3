936681c270c5d044cba08274604d4ce0f4329278
Daniel Meier <me.daniel.meier@gmail.com>
2021-02-08 17:26:49 +0100

    multiple buckets

--- main.tf
+++ main.tf
@@ -5,18 +5,19 @@ locals {
 
 # create s3 bucket
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucketname
-  acl = var.acl
+  bucket          = var.bucketname[count.index]
+  count           = length(var.bucketname)
+  acl             = var.acl
   versioning {
-    enabled = var.versioning
+    enabled       = var.versioning
   }
-  tags = var.tags
+  tags            = var.tags
 }
 
 # generate root and subfolders
 resource "aws_s3_bucket_object" "this" {
     count         = length(var.rootlevelfolder) * length(var.sublevelfolder)
-    bucket        = aws_s3_bucket.this.id
+    bucket        = aws_s3_bucket.this[count.index].id
     acl           = var.acl
     key           = "${element(local.folderstructure, count.index)[0]}/${element(local.folderstructure, count.index)[1]}/"
     content_type  = "application/x-directory"
